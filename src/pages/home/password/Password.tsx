b4554e2234b04644258b1938f7371f71221377b2
niegang <1003018791@qq.com>
2022-05-08 22:14:00 +0800

    save

--- src/pages/home/password/Password.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Password() {
-  return (
-    <>
-      <div style={{width:"410px",height:"40px",color:"white",background:"#221761",lineHeight:"40px",padding:"10px"}}>Password:873878273</div>
-    </>
-  );
-}
